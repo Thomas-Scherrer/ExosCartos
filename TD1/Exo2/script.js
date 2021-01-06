03afc67ebb632d93c534c079f4bfa4c115b299aa
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-06 10:26:32 +0100

    Ajout fonction watchPosition()

--- TD1/Exo2/script.js
+++ TD1/Exo2/script.js
@@ -2,7 +2,8 @@ var options = {
   enableHighAccuracy: true,
 };
 
-function success(position) {
+// Fonction currentPosition()
+function currentPosition(position) {
   var infopos = "Ma position actuelle :<br>";
   infopos += "Latitude : " + position.coords.latitude + "<br>";
   infopos += "Longitude: " + position.coords.longitude + "<br>";
@@ -20,5 +21,25 @@ function error(err) {
   console.warn(`ERREUR (${err.code}): ${err.message}`);
 }
 
+// Fonction watchPosition()
+function surveillePosition(position) {
+  var infopos = "Ma position surveillée :<br>";
+  infopos += "Latitude : " + position.coords.latitude + "<br>";
+  infopos += "Longitude: " + position.coords.longitude + "<br>";
+  infopos +=
+    "Avec une précision de " + position.coords.accuracy + " mètres" + "<br>";
+  infopos +=
+    "Et une vitesse de déplacement de la position égale à " +
+    position.coords.speed +
+    "<br>";
+  infopos += "Voici le timestamp actuel: " + position.timestamp + "<br>";
+  document.getElementById("surveilleposition").innerHTML = infopos;
+}
+
 if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(success, error, options);
+  navigator.geolocation.getCurrentPosition(currentPosition, error, options);
+var survId = navigator.geolocation.watchPosition(
+  surveillePosition,
+  error,
+  options
+);
