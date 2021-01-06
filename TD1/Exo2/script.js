2ad8ed43547d5e1ee80701947f4970419c7c8922
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-06 10:03:58 +0100

    Exo2 - Précision et Vitesse de déplacement

--- TD1/Exo2/script.js
+++ TD1/Exo2/script.js
@@ -1,9 +1,20 @@
-function maPosition(position) {
-  var infopos = "Ma position :\n";
-  infopos += "Latitude : "+position.coords.latitude +"\n";
-  infopos += "Longitude: "+position.coords.longitude+"\n";
+var options = {
+  enableHighAccuracy: true,
+};
+
+function success(position) {
+  var infopos = "Ma position actuelle :<br>";
+  infopos += "Latitude : " + position.coords.latitude + "<br>";
+  infopos += "Longitude: " + position.coords.longitude + "<br>";
+  infopos +=
+    "Avec une précision de " + position.coords.accuracy + " mètres" + "<br>";
+    infopos += "Et une vitesse de déplacement de la position égale à " + position.coords.speed + "<br>";
   document.getElementById("infoposition").innerHTML = infopos;
 }
 
-if(navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(maPosition);
+function error(err) {
+  console.warn(`ERREUR (${err.code}): ${err.message}`);
+}
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(success, error, options);
